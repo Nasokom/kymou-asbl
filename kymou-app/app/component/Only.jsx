6581d7859458fa1a0903c44f60e48661f6ae55c4
Mike <124715146+Nasokom@users.noreply.github.com>
2024-07-15 13:32:27 +0200

    quick

--- kymou-app/app/component/Only.jsx
+++ kymou-app/app/component/Only.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from "@/app/page.module.css"
 import Image from 'next/image'
 import Lottie from "lottie-react";
@@ -10,6 +10,8 @@ const Only = () => {
 
     const [show1,setShow1] = useState(true)
     const [show,setShow] = useState(true)
+
+    const [current,setCurrent] = useState(0)
 
     const images =[
         {
@@ -32,20 +34,45 @@ const Only = () => {
       ]
     
 
+      useEffect(()=>{
+
+
+      },[])
+
+      function changePosition(direction){
+
+        if (direction == '+' && current < 5){
+          setCurrent(current+1)
+        }
+
+        else if(direction == '-' && current > 0){
+          setCurrent(current-1)
+        }
+      }
+
 
   return (
     <main className={styles.main}>
       
+
+
+      <button 
+        onClick={()=>changePosition('-')} 
+        style={{opacity:current>0?'1':'0.2'}}
+        className={styles.arrow}
+      >
+        {'<'}
+      </button>
+
    {show && <div className={styles.helperBox} >
 
    { show1 ? <div className={styles.helper}  onClick={()=>setShow1(false)}>
         
 
-            <h1> Scroll pour parcourir la plaquette</h1>
+            <h1 className={styles.helpText}></h1>
             
             <Lottie animationData={animeLottie} loop={true} />
             <button>OK</button>
-
 
     </div>
     :
@@ -55,6 +82,7 @@ const Only = () => {
             <h1> Zoom pour voir les details</h1>
             
             <Lottie animationData={zoom} loop={true} />
+
             <button>OK</button>
 
     </div>
@@ -62,22 +90,33 @@ const Only = () => {
 
     </div>}
 
-    {images.map((img,i)=>{
+    <div className={styles.imgContainer}>
+      {images.map((img,i)=>{
 
-      return(
-        <div className={styles.imgBox} style={{minWidth:`${img.size*100}vw`}} key={i}>
-        <Image
-          fill
-          src={img.src}
-          alt="Vercel Logo"
-          sizes="100%"
-          className={styles.img}
-          priority
-        />
-        </div>
-      )
-    })}
-          
+        return(
+          <div className={styles.imgBox} 
+          style={{minWidth:`${img.size*100}%`, transform : `translateX(-${current*430}px)`}} 
+          key={i}>
+          <Image
+            fill
+            src={img.src}
+            alt="Vercel Logo"
+            sizes="100%"
+            className={styles.img}
+            priority
+          />
+          </div>
+        )
+      })}
+      </div>
+
+      <button 
+        onClick={()=>changePosition('+')} 
+        style={{opacity:current<5?'1':'0.2'}}
+        className={styles.arrow}
+       >  
+        {'>'}
+        </button>
 
 
   </main>
