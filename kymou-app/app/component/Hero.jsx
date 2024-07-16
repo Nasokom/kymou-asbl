61b31b42936798d44371440daca20614b48562e5
Mike <124715146+Nasokom@users.noreply.github.com>
2024-07-16 16:21:04 +0200

    Update Hero.jsx

--- kymou-app/app/component/Hero.jsx
+++ kymou-app/app/component/Hero.jsx
@@ -15,7 +15,7 @@ const  Hero = async () => {
     const logoLoader = datas.logo ? urlFor(datas.logo)?.url() : '/img/LogoKymou.svg'
 
   return (
-    <div className='h-[100vh] w-full flex relative'>
+    <div className='min-h-[100vh] h-[100vh] w-full flex relative' style={{minHeight:"100vh"}}>
         <Image src={bgLoader} style={{objectFit:'cover', zIndex:0}} fill priority />
 
         <div className='z-10 flex flex-col items-center justify-between w-full h-full p-10'>
