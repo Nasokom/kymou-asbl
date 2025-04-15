7cec25c3c703833069c474170819b030297a07c8
Mike <124715146+Nasokom@users.noreply.github.com>
2025-04-15 20:13:42 +0200

    Update Title.jsx

--- kymou-app/components/Project/Title.jsx
+++ kymou-app/components/Project/Title.jsx
@@ -10,8 +10,8 @@ const TitleProject = () => {
     <>
     <Link href={'/project'} className={`z-[3] group  w-full font-rec1 bg-[--color1] h-[25vh] max-[600px]:h-[20vh] ${usePathname()=='/project' ? 'fixed' :'absolute'} bg-[#ffffff4df]  border-b-4 border-[black] justify-between p-0 text-[5vw] top-0 overflow-hidden`}>       
           {/* <p className='absolute bottom-0 p-0 leading-none'>NOS PROJETS</p>  */}
-        <div className={`h-[5.5vw] overflow-hidden ${usePathname()=='/project' ? 'left-[50%] translate-x-[-50%] delay-700' :'left-2 translate-x-[0%] '} transition-700 duration-300  absolute bottom-0 `}>
-              <div className={`${usePathname()=='/project' ? 'delay-0' : 'translate-y-[-50%]' } duration-300 delay-700 `}>
+        <div className={`h-[5.5vw] overflow-hidden ${usePathname()=='/project' ? 'left-[50%] translate-x-[-50%] delay-[1000ms]' :'left-2 translate-x-[0%] '} transition-700 duration-300  absolute bottom-0 `}>
+              <div className={` duration-300 delay-700  ${usePathname()=='/project' ? 'delay-0 duration-100' : 'translate-y-[-50%]' }`}>
                 <p className={`${usePathname()=='/project' ? '' : "scale-x-[0.25] translate-x-[-37.3%]"} duration-300 ease-out h-5vw leading-none`}>
                     NOS PROJETS
                 </p>
